438d94d25ecc4d41a2512ec1f3b78b03eadc53ec
Samuel Giffard <samuel.giffard@teampicnic.com>
2022-12-17 21:38:16 +0100

    Type hints: `loggers/` (strict mode).

--- report_tool/logger/handlers.py
+++ report_tool/logger/handlers.py
@@ -6,29 +6,48 @@ Could be used by many of IG API related app
 import re
 
 import logging.handlers
+from datetime import time
 from pathlib import Path
+from typing import Literal
 
 from report_tool.utils.constants import get_logs_dir
-
-try:
-    import codecs
-except ImportError:
-    codecs = None
 
 RE_FILENAME = re.compile(r"(\w)*(-)")
 
 
 class ReportToolFileHandler(logging.handlers.TimedRotatingFileHandler):
-    """
-    Custom TimedRotatingFileHandler, as the name of log created
-    in base class in not convenient(an extension is appended to
-    the base file name). Subclass getFilesToDelete as we change
-    of log file. Looks complicated for a simple goal
+    """Custom TimedRotatingFileHandler.
+
+    This class is used to create a custom TimedRotatingFileHandler that
+    prepends the filename with the log directory.
     """
 
-    def __init__(self, filename: str | Path, *args, **kwargs):
+    def __init__(
+        self,
+        filename: str | Path,
+        when: Literal[
+            "S", "M", "H", "D", "midnight", "W0", "W1", "W2", "W3", "W4", "W5", "W6"
+        ] = "H",
+        interval: int = 1,
+        backupCount: int = 0,
+        encoding: str | None = None,
+        delay: bool = False,
+        utc: bool = False,
+        atTime: time | None = None,
+        errors: str | None = None,
+    ) -> None:
         """Prepend the filename with the log directory."""
         filepath = Path(filename)
         if not filepath.is_absolute():
             filepath = get_logs_dir() / filepath
-        super().__init__(filepath, *args, **kwargs)
+        super().__init__(
+            filepath,
+            when=when,
+            interval=interval,
+            backupCount=backupCount,
+            encoding=encoding,
+            delay=delay,
+            utc=utc,
+            atTime=atTime,
+            errors=errors,
+        )
