428554b928ade1ecb5d097d520b4c17b479cf571
Samuel Giffard <samuel.giffard@teampicnic.com>
2022-12-17 20:07:43 +0100

    Type hints: `json_utils.py`.

--- report_tool/utils/json_utils.py
+++ report_tool/utils/json_utils.py
@@ -1,7 +1,8 @@
 import json
-import datetime
+from datetime import datetime, date, time
 from decimal import Decimal
 import logging
+from typing import Any, TypedDict, Mapping, TypeVar
 
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%H:%M:%S.%f"
@@ -11,19 +12,26 @@ DATETIME_FORMAT = f"{DATE_FORMAT} {TIME_FORMAT}"
 logger = logging.getLogger(__name__)
 
 
+class EncodedValue(TypedDict):
+    """A value that can be encoded in JSON."""
+
+    _type: str
+    value: str
+
+
 class RoundTripEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
+    def default(self, obj: Any) -> EncodedValue:
+        if isinstance(obj, datetime):
             return {
                 "_type": "datetime.datetime",
                 "value": obj.strftime(DATETIME_FORMAT),
             }
-        if isinstance(obj, datetime.date):
+        if isinstance(obj, date):
             return {
                 "_type": "datetime.date",
                 "value": obj.strftime(DATE_FORMAT),
             }
-        if isinstance(obj, datetime.time):
+        if isinstance(obj, time):
             return {
                 "_type": "datetime.time",
                 "value": obj.strftime(TIME_FORMAT),
@@ -33,35 +41,39 @@ class RoundTripEncoder(json.JSONEncoder):
                 "_type": "decimal.Decimal",
                 "value": str(obj),
             }
-        return super(RoundTripEncoder, self).default(obj)
+        return super().default(obj)
+
+
+InputT = TypeVar("InputT", bound=Mapping[str, Any])
 
 
 class RoundTripDecoder(json.JSONDecoder):
     def __init__(self, *args, **kwargs):
         json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
 
-    def object_hook(self, obj):
+    @staticmethod
+    def object_hook(obj: InputT) -> InputT | datetime | date | time | Decimal:
         if "_type" not in obj:
             return obj
-        type = obj["_type"]
-        if type == "datetime.datetime":
-            return datetime.datetime.strptime(obj["value"], DATETIME_FORMAT)
-        if type == "datetime.date":
-            return datetime.datetime.strptime(obj["value"], DATE_FORMAT).date()
-        if type == "datetime.time":
-            return datetime.datetime.strptime(obj["value"], TIME_FORMAT).time()
-        if type == "decimal.Decimal":
+        type_ = obj["_type"]
+        if type_ == "datetime.datetime":
+            return datetime.strptime(obj["value"], DATETIME_FORMAT)
+        if type_ == "datetime.date":
+            return datetime.strptime(obj["value"], DATE_FORMAT).date()
+        if type_ == "datetime.time":
+            return datetime.strptime(obj["value"], TIME_FORMAT).time()
+        if type_ == "decimal.Decimal":
             return Decimal(obj["value"])
-        logger.warning(f"Unknown type for Json Decoded: {type}.")
+        logger.warning(f"Unknown type for Json Decoded: {type_}.")
         return obj
 
 
 if __name__ == "__main__":
     data = {
         "name": "Report O'Toole",
-        "dt": datetime.datetime.now(),
-        "d": datetime.datetime.now().date(),
-        "t": datetime.datetime.now().time(),
+        "dt": datetime.now(),
+        "d": datetime.now().date(),
+        "t": datetime.now().time(),
         "value": Decimal("42.23"),
     }
     json_str = json.dumps(data, cls=RoundTripEncoder, indent=2)
